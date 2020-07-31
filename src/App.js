401826c15de60b0be948f8f27327c6c8ca54dcf0
Bryan Roof <broof716@gmail.com>
2020-07-31 06:24:23 -0700

    created a pages folder

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import React from 'react';
 import Navigation from './components/navigation'
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
 
+import PageRenderer from './page-renderer'
+
 function App() {
   return (
     <Router>
       <div className="App">
         <Navigation />
+        <Switch>
+          <Route path="/:page" component={PageRenderer} />
+          <Route path="/" render={() => <Redirect to="/home" />} />
+          <Route component={() => 404}v/>
+        </Switch>
       </div>
     </Router>
   );
