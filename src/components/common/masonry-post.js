2a65aec668d005c665abf2938fb15d48b83a4d25
Bryan Roof <broof716@gmail.com>
2020-08-09 11:50:22 -0700

    error code after starting masonry-post.js

--- src/components/common/masonry-post.js
+++ src/components/common/masonry-post.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export default function MasonryPost ({post, tagsOnTop}) {
-  const style = {backgroundImage: `url("${require(`../../assets/images/${post.images}`)}")`};
+  const style = {backgroundImage: `url("${require(`../../assets/images/${post.image}`)}")`};
 
   return (
     <a className="masonry-post overlay" style={style} href={post.link}>
